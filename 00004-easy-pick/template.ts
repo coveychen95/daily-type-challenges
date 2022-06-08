4f91d6a57c5c70df0e1738f3ec6a75aff1648070
gang.chen1 <gang.chen1@yintech.cn>
2022-06-08 17:53:05 +0800

    feat: pick

---
+++ 00004-easy-pick/template.ts
@@ -0,0 +1,17 @@
+type MyPick<T, K extends keyof T> = { [P in K]: T[P]; }
+
+
+// For example
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+
+const todo: TodoPreview = {
+    title: 'Clean room',
+    completed: false,
+}
