f323548a6bb852b0e809ac9199847e920ff435c3
coveychen95 <coveychen95@outlook.com>
2022-07-13 22:37:57 +0800

    feat: readonly 2

--- 00008-medium-readonly-2/template.ts
+++ 00008-medium-readonly-2/template.ts
@@ -1,3 +1,6 @@
 // 1. 处理两个对象
 // T 中 匹配 K ，匹配到设置为 readonly
-type MyReadonly2<T, K> = any
+// ??? K extends keyof T = keyof T
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [P in K]: T[P]
+} & Omit<T, K>
