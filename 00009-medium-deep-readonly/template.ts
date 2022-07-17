d813c003434b4fdde01ee71d46b57fd5684ae975
coveychen95 <coveychen95@outlook.com>
2022-07-17 22:15:36 +0800

    feat: 1

--- 00009-medium-deep-readonly/template.ts
+++ 00009-medium-deep-readonly/template.ts
@@ -1,1 +1,8 @@
-type DeepReadonly<T> = any
+// 1. 递归
+//    对象 or 数组 的深层次递归
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: keyof T[K] extends never 
+    ? T[K] 
+    : DeepReadonly<T[K]>
+}
+
