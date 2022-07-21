15d2b6bbd4530bd33bad4f82d07aeb523b8f7e61
coveychen95 <coveychen95@outlook.com>
2022-07-22 00:00:43 +0800

    feat:v1

--- 00012-medium-chainable-options/template.ts
+++ 00012-medium-chainable-options/template.ts
@@ -1,4 +1,12 @@
+type Merge<T, U> = {
+  [P in Exclude<keyof T, keyof U>]: T[P]
+} & {
+  [N in keyof U]: U[N]
+}
+
 type Chainable<T = {}> = {
-  option(key: string, value: any): any
-  get(): T
+  // option<K extends string, V extends any>(key: K extends keyof T ? never : K, value: V): Chainable<T & Record<K, V>>
+  option<K extends string, V>(key: K, value: V): Chainable<Merge<T, {[P in K]: V}>>
+  // option<K extends string, V>(key: K, value: V): Chainable<T & { [key in K]: V }>,
+  get(): T 
 }
